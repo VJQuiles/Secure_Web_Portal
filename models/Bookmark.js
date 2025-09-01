9a245e13a59f208c2866e45e0569d2e26405ab99
Vince Quiles <vjquiles@msn.com>
2025-09-01 10:36:47 -0400

    Bookmark mode created, mirrored the note model in my secure record storage repo. Also in the last commit i didn't mention, I got rid of the admin.js file. After rereading requirements, i felt it wasn't necessary to include.

---
+++ models/Bookmark.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const bookmarkSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        }
+    }
+)
+const Bookmark = mongoose.model('Bookmark', bookmarkSchema)
+
+module.exports = Bookmark
--- models/User.js
+++ models/User.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-const { Schema } = mongoose
 
-const userSchema = new Schema(
+const userSchema = new mongoose.Schema(
     {
         username: {
             type: String,
