a4238ebc7b745a8bb6ea097bb3a648f9e1ec1850
Vince Quiles <vjquiles@msn.com>
2025-09-01 20:31:13 -0400

    Routes mapped out, now for controller logic. Also borrowing line 6 in the bookmarkRoutes.js file from the Secure_Record_Storage repo, in the notes route. Rather than writing out verifyUser in all 5 routes.

---
+++ routes/apiRoutes/bookmarkRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const Bookmark = require('../../models/Bookmark')
+const bookmarkController = require('../../controllers/bookmarkController')
+const verifyUser = require('../../middleware/auth')
+
+router.use(verifyUser)
+
+router.post('/create-bookmark', bookmarkController.createBookmark)
+router.get('/user-bookmarks', bookmarkController.getAllBookmarks)
+router.get('/user-bookmarks/:id', bookmarkController.getSingleBookmark)
+router.put('/update-bookmark/:id', bookmarkController.updateBoookmark)
+router.delete('delete-bookmark/:id', bookmarkController.deleteBookmark)
